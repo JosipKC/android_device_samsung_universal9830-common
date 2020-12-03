f11780d24a34b7c48c579d3afd377f3d8c3f6cdb
JosipKC <d3hum4niz3d@gmail.com>
2020-12-03 12:25:19 +0100

    build nfc stuff
    Change-Id: I824e1f3a643a26807579314da3edd37c6190a95a

--- platform/product_packages.mk
+++ platform/product_packages.mk
@@ -96,6 +96,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtextclassifier_hash.vendor
 
+### NFC
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    NfcNci \
+    Tag
+
 ### OMX
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
