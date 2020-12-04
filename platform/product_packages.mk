d250d5784e3e70790c887672857342e8a211acd4
JosipKC <d3hum4niz3d@gmail.com>
2020-12-04 15:17:14 +0100

    add vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.samsung
    Change-Id: Ic5d64f485d4cb84253de75c4ac43efa5a7909e2b

--- platform/product_packages.mk
+++ platform/product_packages.mk
@@ -168,4 +168,6 @@ PRODUCT_PACKAGES += \
 
 ### FINGERPRINT
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.samsung
+    android.hardware.biometrics.fingerprint@2.1-service.samsung \
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.samsung
+
